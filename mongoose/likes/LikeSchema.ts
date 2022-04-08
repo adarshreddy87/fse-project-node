1996bcd5c769f588813b1a611d23e4605f1053ad
Juan Ong <juanong26@gmail.com>
2022-04-08 14:16:56 -0400

    bug fix - get likes and dislikes to load consistently

--- mongoose/likes/LikeSchema.ts
+++ mongoose/likes/LikeSchema.ts
@@ -12,6 +12,6 @@ import Like from "../../models/likes/Like";
 const LikeSchema = new mongoose.Schema<Like>({
     tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
     // change this to any to avoid errors
-    likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"}
+    likedBy: {type: String, ref: "UserModel"}
 }, {collection: "likes"});
 export default LikeSchema;
