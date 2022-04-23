f78de56587d6db818ba86f1f91575c1e940b1842
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-04-23 17:29:47 -0400

    Implemented new endpoints in Lists Controller.

--- controllers/ListController.ts
+++ controllers/ListController.ts
@@ -4,6 +4,8 @@
 import {Request, Response, Express} from "express";
 import ListControllerI from "../interfaces/ListControllerI";
 import ListDao from "../daos/ListDao";
+import TuitDao from "../daos/TuitDao";
+import UserDao from "../daos/UserDao";
 
 /**
  * @class ListController Implements RESTful web service API for lists resource
@@ -18,6 +20,8 @@ import ListDao from "../daos/ListDao";
  */
 export default class ListController implements ListControllerI {
     private static listDao: ListDao = ListDao.getInstance();
+    private static tuitDao: TuitDao = TuitDao.getInstance();
+    private static userDao: UserDao = UserDao.getInstance();
     private static listController: ListController | null = null;
     /**
      * Create a single instance of the list controller
@@ -28,6 +32,8 @@ export default class ListController implements ListControllerI {
         if (ListController.listController === null) {
             ListController.listController = new ListController();
             app.get('/api/users/:uid/lists', ListController.listController.getAllUsersInList);
+            app.get('/api/users/:uid/not/lists', ListController.listController.getAllUsersNotInList);
+            app.get('/api/users/:uid/lists/tuits', ListController.listController.getAllUsersTuitsInList);
             app.post('/api/users/:uid/lists/:userId', ListController.listController.addUserToList);
             app.delete('/api/users/:uid/lists/:userId', ListController.listController.removeUserFromList);
         }
@@ -75,5 +81,62 @@ export default class ListController implements ListControllerI {
     addUserToList = (req: Request, res: Response) => {
         ListController.listDao.addUserToList(req.params.uid, req.params.userId)
             .then(status => res.send(status));
+    }
+
+    /**
+     * Retrieves all the users not in the user list from the database
+     * @param {Request} req The request from the client, including the path parameter uid representing the user
+     * id of the logged in user.
+     * @param {Response} res The response to the client, including the body as a JSON array containing the
+     * relevant user objects
+     */
+    getAllUsersNotInList = async (req: Request, res: Response) => {
+        const uid = req.params.uid;
+        // @ts-ignore
+        const profile = req.session['profile'];
+        const userId = uid === "me" && profile ? profile._id : uid;
+        let users : any[] = [];
+        let usersList : any[] = [];
+        let usersNotInList: any[] = [];
+        users = await ListController.userDao.findAllUsers();
+        usersList = await ListController.listDao.getAllUsersInList(userId);
+        let usersListIds: any[] = [];
+        for (let user of usersList) {
+            usersListIds.push(user.addedUser._id.toString())
+        }
+        for (let user of users) {
+            if (!(usersListIds.includes(user._id.toString()))) {
+                usersNotInList.push(user)
+            }
+        }
+        return res.json(usersNotInList);
+    }
+
+    /**
+     * Retrieves all the tuits of the users in the list from the database
+     * @param {Request} req The request from the client, including the path parameter uid representing the user
+     * id of the logged in user.
+     * @param {Response} res The response to the client, including the body as a JSON array containing the
+     * relevant tuit objects
+     */
+    getAllUsersTuitsInList = async (req: Request, res: Response) => {
+        const uid = req.params.uid;
+        // @ts-ignore
+        const profile = req.session['profile'];
+        const userId = uid === "me" && profile ? profile._id : uid;
+        let usersList: any[] = [];
+        let usersListIds : any[] = [];
+        let tuitsList: any[] = [];
+        usersList = await ListController.listDao.getAllUsersInList(userId);
+        for (let user of usersList) {
+            usersListIds.push(user.addedUser._id)
+        }
+        for (let userId of usersListIds) {
+            let tuit = await ListController.tuitDao.findAllTuitsByUser(userId)
+            if (!(tuit.length == 0)) {
+                tuitsList.push(tuit)
+            }
+        }
+        return res.json(tuitsList);
     }
 };
--- interfaces/ListControllerI.ts
+++ interfaces/ListControllerI.ts
@@ -4,4 +4,6 @@ export default interface ListControllerI {
     getAllUsersInList (req: Request, res: Response): void;
     removeUserFromList (req: Request, res: Response): void;
     addUserToList (req: Request, res: Response): void;
+    getAllUsersNotInList (req: Request, res: Response): void;
+    getAllUsersTuitsInList (req: Request, res: Response): void;
 }
